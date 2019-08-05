889004b924066158829c71468f6c2b66e27d4fca
Jose Montero <josemariodev@MacBookPro.attlocal.net>
2019-08-05 14:36:12 -0500

    project setup, GET working

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+const users = require('./data/db.js');
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('getting data successfully!');
+});
+
+
+
+const port = 8000;
+server.listen(port, () => console.log('running on port 8000...'));
