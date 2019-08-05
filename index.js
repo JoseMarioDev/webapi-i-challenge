fca3d46db7ad5e4badf8847c35079e03ffc0fab1
Jose Montero <josemariodev@MacBookPro.attlocal.net>
2019-08-05 15:25:00 -0500

    POST is working

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 // implement your API here
 const express = require('express');
-const users = require('./data/db.js');
+const Users = require('./data/db.js');
 const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send('getting data successfully!');
 });
+
+server.get('/api/users', (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'err getting list of users' });
+    });
+});
+
+server.post('/api/users', (req, res) => {
+  const userInfo = req.body;
+  Users.insert(userInfo)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'error adding user' });
+    });
+});
+
+
 
 
 
